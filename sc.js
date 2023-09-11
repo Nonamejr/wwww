f06c37d0a61744c413ccee4284a659d6de609bc7
Nonamejr <pashashamaev14@gmail.com>
2023-09-11 17:31:04 +0300

    g

---
+++ sc.js
@@ -0,0 +1,118 @@
+/*str, my_str, myStr,a1, $, $а допустимые имена переменных*/
+
+// Переменные 
+// Ключевое слово var, let, const 
+// Формат LowerCamelCase (верблюжья нотация)
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(second);
+
+// Типы данных, классификация
+
+// Примитивный тип данных
+
+//Строка string 
+// двойные кавычки, одинарные кавычки-простые, обратные-имеют расширенный функционал
+
+// let string = "hello';
+// console.log(string);
+// let name = "Иван"; 
+// console.log(name);
+
+//Числа number
+
+//let number = 5;
+//console.log(number);
+
+//Специальный тип данных null
+//null// специальное значение, которое представляет отсутсутствующий объект
+
+// console.log(something);
+
+//undefined //представляет значение переменной, которая не была инициализирована
+
+// let numb;
+// console.log(numb);
+
+//NaN //специальное значение "не число", которая говорит о том, что была выполнена бессмысленная операция
+
+//console.log('string' * 4);
+
+//Infinity //бесконечность
+
+//console.log(4/0);
+
+//Логический тип данных boolean (true, false)
+
+//let boolean = true;
+//console.log(boolean);
+
+// Абстрактный тип данных object(}, это набор свойст и значений
+
+//let obj = {};
+
+//let persone = {
+//    name: 'kseniya',
+//    age: 14,
+//    icMarriered: false
+//};
+
+//console.log(persone['name']);
+
+//Массив
+
+//let arr = ['plum.jpeg', 'apple.bmp', 'orange.png'];
+//console.log(arr[0]);
+
+//Простое общение с пользователем
+
+alert("hello, world!");
+
+//let answer = confirm('How are you');
+//console.log(answer);
+
+//let answer = promt('18 yesti??????', 'da')
+//console.log(answer)
+
+// оператор typeof, для определения типа данных
+// let answer = prompt('Есть ли вам 18 лет?', 'Да');
+// console.log(typeof(answer));
+// оПЕРАТОРЫ js
+// унарный +
+
+// console.log('arr' + ' - object');
+// console.log(4 + ' - object');
+
+//Инкремент и декремент
+//++префиксный инкремент
+// --постфиксный инкремент
+
+//let post = 10;
+//    decr = 10;
+
+//    console.log(incr++);
+//    console.log(--decr);
+
+//Остаток от деления
+
+// console.log(5%2);
+
+//= присваивание 
+// == проверка на равенство
+
+//console.log('2' == 2);
+
+
+//оператор && (и)
+
+//let isChecked = true;
+//    isClosed = true;
+//consolelog(isChecked && isClosed);
+
+// оператор || (или)
+
+let isChecked = true;
+    isClosed = true;
+consolelog(isChecked || isClosed);
